5a44252b8a1a9950107377a75825f4f7f7a7f70d
ikuza <97811292+IkuzaDev@users.noreply.github.com>
2025-09-15 00:21:30 +0700

    anoboy->kuramanime

--- src/utils/movies.js
+++ src/utils/movies.js
@@ -1,28 +1,35 @@
 import { load } from 'cheerio';
 import axios from 'axios';
+import fs from 'fs';
 import pagination from '../lib/pagination.js';
-const ANOBOY = process.env.ANOBOY || 'https://ww3.anoboy.app';
+const ANOBOY = process.env.ANOBOY || 'https://v8.kuramanime.tel/';
 const movies = async (page = 1) => {
-    console.log(`${ANOBOY}/category/anime-movie/page/${page}`);
-    const { data } = await axios.get(`${ANOBOY}/category/anime-movie/page/${page}`);
-    const $ = load(data);
+    console.log(`${ANOBOY}quick/movie?order_by=latest&page=${page}`);
+    const { data } = await axios.get(`${ANOBOY}quick/movie?order_by=latest&page=${page}`,
+        {headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36',
+            'Accept-Language': 'en-US,en;q=0.9'
+        }}
+    );
+    let $ = load(data);
+    fs.writeFileSync('anime.html', data, 'utf8');
     const movies = [];
-    $('a[rel="bookmark"]').each((index, element) => {
+    $('div[class="col-lg-4 col-md-6 col-sm-6"]').each((index, element) => {
         const $ = load(element);
-        const animex = $('a[rel="bookmark"]').attr('href')?.replace(ANOBOY, '').split('/');
+        const animex = $('a').first().attr('href')?.replace(ANOBOY, '').split('/');
+        console.log(animex)
         movies.push({
-            title: $('a[rel="bookmark"]').attr('title'),
-            years: animex[1],
-            month: animex[2],
-            slug: animex[3],
-            poster: ANOBOY + $('amp-img').attr('src'),
-            otakudesu_url: $('a[rel="bookmark"]').attr('href')
+            title: $('h5 a').text().trim(),
+            code: animex[1],
+            slug: animex[2],
+            poster: $('.product__item__pic.set-bg').attr('data-setbg'),
+            otakudesu_url: $('a').first().attr('href')
         });
     });
     
     return {
         movies,
-        pagination: pagination($('.wp-pagenavi').toString(), true)
+        pagination: pagination($('div.product__pagination').toString(), true)
     }
 };
 export default movies;
