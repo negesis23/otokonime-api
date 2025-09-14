324a956d1bdaef9b72a7f6c8e6493419b3a27439
ikuza <97811292+IkuzaDev@users.noreply.github.com>
2025-09-15 00:22:35 +0700

    anoboy -> kuramanime
    thx to Sanka Vollereii:)

--- src/utils/movie.js
+++ src/utils/movie.js
@@ -1,41 +1,35 @@
 import { load } from 'cheerio';
+import fs from 'fs';
 import axios from 'axios';
-import {getDownloadLink, getToken, getContent, getWt, listFiles} from 'gofile-downloader';
-const ANOBOY = process.env.ANOBOY || 'https://ww3.anoboy.app';
+const ANOBOY = process.env.ANOBOY || 'https://v8.kuramanime.tel';
 const movie = async (slug) => {
-    console.log(`${ANOBOY}${slug}`);
-    const { data } = await axios.get(`${ANOBOY}${slug}`);
-    const $ = load(data);
-    const movie = {};
-    const sinopsi = [];
-    movie.title = $('.unduhan h3').text().toLowerCase().split('sub')[0];
-    movie.poster = ANOBOY + $('.unduhan amp-img').attr('src');
-
-    const downloadUrls = {
-        '480p': [],
-        '720p': [],
-        '1080p': []
-    };
-    $('.download .ud .udl').each((index, element) => {
-        const $$ = load(element);
-        const label = $$.text().trim().toLowerCase();
-        const link = $$('a').attr('href');
-        console.log(label, link);
-
-        if (!link) return;
-
-        if (label.includes('480')) {
-            if(link !== 'none') downloadUrls['480p'].push(link);
-        } else if (label.includes('720')) {
-            if(link !== 'none') downloadUrls['720p'].push(link);
-        } else if (label.includes('1k')) {
-            if(link !== 'none') downloadUrls['1080p'].push(link);
+    console.log(`${ANOBOY}/anime${slug}`);
+    const { data } = await axios.get(`${ANOBOY}/anime${slug}`,{
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36',
+            'Accept-Language': 'en-US,en;q=0.9'
         }
     });
-    movie.download_urls = downloadUrls;
-    var stream =  downloadUrls['480p'].map((item) => item.includes('mp4upload') ? item : null).filter((item) => item !== null)[0];
-    movie.stream_url = stream;
+    const $ = load(data);
+    const movie = {};
+    movie.title = $('.anime__details__title h3').text().toLowerCase().split('sub')[0];
+    movie.poster = $('.anime__details__pic.set-bg').attr('data-setbg');
+    movie.sinopsi = $('#synopsisField').text().trim();
+    //https://www.sankavollerei.com/anime/kura/watch/3138/dandadan/1
+    const animex = await axios.get(`https://www.sankavollerei.com/anime/kura/watch${slug}/1`,{
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+            'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Cache-Control': 'no-cache',
+            'Referer': 'https://www.google.com/',
+            'Connection': 'keep-alive',
+        }
+    });
+    movie.download_urls = animex.data.video;
+    movie.stream_url = animex.data.video;
 
     return movie;
 };
 export default movie;
+
